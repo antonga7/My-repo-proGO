3abafc52692ad7bf0d5042c3789b11d691d3bbaf
Антон Галкин <antongalkin@MacBook-Air-Anton.local>
2025-09-28 22:47:34 +0300

    Тест хендлера и кэширование

--- internal/handler/http.go
+++ internal/handler/http.go
@@ -7,12 +7,15 @@ import (
 	"net/http"
 	"proGO/internal/fib"
 	"strconv"
+	"sync"
 )
 
 type Response struct {
 	N   int      `json:"n"`
 	Fib *big.Int `json:"fib"`
 }
+
+var fibCache = sync.Map{}
 
 func WriteJSONError(w http.ResponseWriter, msg string, code int) {
 	log.Printf("Ошибка [%d]: %s", code, msg)
@@ -34,19 +37,29 @@ func FibHandler(w http.ResponseWriter, r *http.Request) {
 		WriteJSONError(w, "параметр 'n' должен быть целым числом >= 0", http.StatusBadRequest)
 		return
 	}
+
+	if val, ok := fibCache.Load(n); ok {
+		fib := val.(*big.Int)
+		resp := Response{N: n, Fib: fib}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			log.Printf("Ошибка при кодировании ответа: %v", err)
+		}
+
+	}
+
 	result, err := fib.Fibonacci(n)
 	if err != nil {
 		WriteJSONError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	fibCache.Store(n, result)
 	resp := Response{N: n, Fib: result}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
-
 		log.Printf("Ошибка при кодировании ответа: %v", err)
 	}
-
-	log.Printf("Успешно: n=%d fib=%s", n, result.String())
+	log.Printf("Успешно и сохранено в кэш: n=%d fib=%s", n, result.String())
 }
